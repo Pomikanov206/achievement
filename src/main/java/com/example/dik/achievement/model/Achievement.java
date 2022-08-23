f3e3a6ba2c44c37f0657895432b5a175e89c8e11
Pomikanov206 <pomikanov@gmail.com>
2022-08-23 14:54:43 +0300

    Add Entity classes: Student and Achievement. Create Many to Many association

--- src/main/java/com/example/dik/achievement/model/Achievement.java
+++ src/main/java/com/example/dik/achievement/model/Achievement.java
@@ -1,25 +1,33 @@
 package com.example.dik.achievement.model;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "achievement_list")
 public class Achievement {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "achievement_id")
     private int id;
     private String title;
     private String description;
     private String imageUrl;
+    private boolean isActivated;
+    @ManyToMany(mappedBy = "achievements")
+    private Set<Student> students = new HashSet<>();
 
     public Achievement() {
     }
 
-    public Achievement(int id, String title, String description, String imageUrl) {
+    public Achievement(int id, String title, String description, String imageUrl, boolean isActivated, Set<Student> students) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.imageUrl = imageUrl;
+        this.isActivated = isActivated;
+        this.students = students;
     }
 
     public int getId() {
@@ -52,5 +60,21 @@ public class Achievement {
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    public boolean isActivated() {
+        return isActivated;
+    }
+
+    public void setActivated(boolean activated) {
+        isActivated = activated;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
     }
 }
--- src/main/java/com/example/dik/achievement/model/Student.java
+++ src/main/java/com/example/dik/achievement/model/Student.java
@@ -1,12 +1,15 @@
 package com.example.dik.achievement.model;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "students")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "student_id")
     private int id;
     @Column(name = "student_name")
     private String name;
@@ -15,6 +18,53 @@ public class Student {
     /*
     Achievements save in JSON format
      */
-    @Column(name = "achievement_list")
-    private String achievements;
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "Student_Achievement",
+            joinColumns = { @JoinColumn(name = "student_id")},
+            inverseJoinColumns = {@JoinColumn(name = "achievement_id")})
+    //@Column(name = "achievement_list")
+    private Set<Achievement> achievements = new HashSet<>();
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String group, Set<Achievement> achievements) {
+        this.id = id;
+        this.name = name;
+        this.group = group;
+        this.achievements = achievements;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Set<Achievement> getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(Set<Achievement> achievements) {
+        this.achievements = achievements;
+    }
 }
