5290cc30bc28dbfa65fb34a8a71b947667de64bb
Pomikanov206 <pomikanov@gmail.com>
2022-08-28 02:39:32 +0300

    Add basic routing

--- src/main/ng/achievement-client/src/app/app.module.ts
+++ src/main/ng/achievement-client/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { CreateStudentComponent } from './create-student/create-student.component';
 import { StudentAchievementComponent } from './student-achievement/student-achievement.component';
+
+const appRoutes: Routes =[
+  { path: 'find-student', component: StudentListComponent },
+  { path: 'add-student', component: CreateStudentComponent },
+  { path: 'achievement', component: StudentAchievementComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +24,8 @@ import { StudentAchievementComponent } from './student-achievement/student-achie
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
